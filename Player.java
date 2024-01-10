42be0ffca05a2cbd8f911c8d256d718c2c486606
TerryT1 <144486954+TerryT1@users.noreply.github.com>
2024-01-10 15:25:11 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,42 +1,80 @@
 import java.util.Scanner;
 
-public class Player {
-  private String name;
-  private int position;
+public class ConnectFourGame {
+  private static final int ROWS = 6;
+  private static final int COLS = 7;
+  private static final char EMPTY = '-';
+  private char[][] board = new char[ROWS][COLS];
+  private char currentPlayer;
   
-  public Player(String name) {
-    this.name = name;
+  public ConnectFourGame() {
+    initializeBoard();
+    currentPlayer = 'X';
   }
   
-  public String getName() {
-    return name;
+  private void initializeBoard() {
+    for (int i = 0; i < ROWS; i++){
+      for (int j = 0; j < COLS; j++){
+        board[i][j] = EMPTY;
+      }
+    }
   }
   
-  public int getPosition() {
-    return position;
+  private void printBoard() {
+    for (char[] row : board) {
+      for (char cell : row) {
+        System.out.print(cell + " ");
+      }
+      System.out.println();
+    }
+    System.out.println();
   }
   
-  public void move() {
-    position++;
+  private boolean isValidMove(int column) {
+    return column >= 0 && column < COLS && board[0][column] == EMPTY;
   }
   
-  public void answerQuestion(Question question, Scanner scanner) {
-    System.out.print(name + ": " + question.getExpression() + " = ");
-    long startTime = System.currentTimeMillis();
-    int answer = scanner.nextInt();
-    long endTime = System.currentTimeMillis();
-    question.setAnswerTime(endTime - startTime);
-    // Consume the newline character
-    scanner.nextLine();
+  private void dropToken(int column) {
+    for (int i = ROWS - 1; i >= 0; i--) {
+      if (board[i][column] == EMPTY) {
+        board[i][column] = currentPlayer;
+        break;
+      }
+    }
   }
   
-  public boolean isFasterThan(Player otherPlayer) {
-    return this.getPosition() == otherPlayer.getPosition() && this.getAnswerTime() < otherPlayer.getAnswerTime();
+  private boolean checkForWin() {
+    // TODO: Implement the logic to check for a win
+    // You can use similar logic as in the previous example
+    
+    return false;
   }
   
-  
-  public long getAnswerTime() {
-    // Dummy method; for demonstration purposes
-    return 21;
+  public void play() {
+    Scanner scanner = new Scanner(System.in);
+    
+    while (true) {
+      System.out.println("Current board:");
+      printBoard();
+      
+      System.out.println("Player " + currentPlayer + "'s turn. Enter column (1-" + COLS + "): ");
+      int column = scanner.nextInt() - 1;
+      
+      if (isValidMove(column)) {
+        dropToken(column);
+        
+        if (checkForWin()) {
+          System.out.println("Player " + currentPlayer + " wins!");
+          break;
+        }
+        
+        // Switch players
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+      } else {
+        System.out.println("Invalid move. Try again.");
+      }
+    }
   }
 }
+
+
