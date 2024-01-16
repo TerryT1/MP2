5b2ba38d955794eae08db590416a5804357421ca
TerryT1 <144486954+TerryT1@users.noreply.github.com>
2024-01-15 20:54:38 -0500

    Create Object.java

---
+++ Object.java
@@ -0,0 +1,16 @@
+//object reference requirement
+public class Object {
+    private int value;
+
+    public Object(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
